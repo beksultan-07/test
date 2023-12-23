e011f46fe9fcfde127a0379417e761b3df974869
Beksultan Bakytov <beksultanbakytov@MacBook-Air-Beksultan.local>
2023-12-23 17:26:52 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+
+import { Button, Flex } from "antd";
+
+function App() {
+    return (
+        <Flex>
+            <Button>click</Button>
+            <Button>click</Button>
+        </Flex>
+    );
+}
+
+export default App;
