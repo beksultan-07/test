5dc325684b25c33407c0da627c869aa0846ab842
Beksultan Bakytov <beksultanbakytov@MacBook-Air-Beksultan.local>
2023-12-23 23:57:41 +0600

    added address content

--- src/components/address/index.tsx
+++ src/components/address/index.tsx
@@ -1,7 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Address = () => {
-    return <div>Address</div>;
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Divider, Flex } from "antd";
+import {
+    ArrowRightOutlined,
+    ClockCircleOutlined,
+    EnvironmentOutlined,
+    PhoneOutlined,
+} from "@ant-design/icons";
+
+import logo from "../../assets/images/address.svg";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./style.scss";
+
+const Address: React.FC = () => {
+    const [addresses] = useState([
+        {
+            id: 1,
+            logo,
+            name: "Iskender home",
+            url: "https://maps.app.goo.gl/jFCpTNQjCxKRsYqz6",
+            address: "Кыргызстан, г. Бишкек ул. Турусбекова A167",
+            workTime: "08:00 - 22:00",
+            phoneNumber: "+996 500 345 345",
+        },
+        {
+            id: 3,
+            logo,
+            name: "Iskender home",
+            url: "https://maps.app.goo.gl/jFCpTNQjCxKRsYqz6",
+            address: "Кыргызстан, г. Бишкек ул. Турусбекова A167",
+            workTime: "08:00 - 22:00",
+            phoneNumber: "+996 500 345 345",
+        },
+        {
+            id: 3,
+            logo,
+            name: "Iskender home",
+            url: "https://maps.app.goo.gl/jFCpTNQjCxKRsYqz6",
+            address: "Кыргызстан, г. Бишкек ул. Турусбекова A167",
+            workTime: "08:00 - 22:00",
+            phoneNumber: "+996 500 345 345",
+        },
+        {
+            id: 4,
+            logo,
+            name: "Iskender home",
+            url: "https://maps.app.goo.gl/jFCpTNQjCxKRsYqz6",
+            address: "Кыргызстан, г. Бишкек ул. Турусбекова A167",
+            workTime: "08:00 - 22:00",
+            phoneNumber: "+996 500 345 345",
+        },
+    ]);
+
+    return (
+        <section className="address">
+            <div className="container">
+                <h2 className="title">Address</h2>
+
+                <Swiper
+                    modules={[Pagination]}
+                    pagination={{ clickable: true, dynamicBullets: true }}
+                    spaceBetween={10}
+                    slidesPerView={3}
+                    loop
+                >
+                    {addresses.map((address) => (
+                        <SwiperSlide>
+                            <div className="address__item">
+                                <a
+                                    className="address__item__top"
+                                    href={address.url}
+                                    target="_blank"
+                                >
+                                    <Flex
+                                        gap={10}
+                                        align="center"
+                                        justify="space-between"
+                                    >
+                                        <img src={address.logo} alt="" />
+
+                                        <Flex vertical>
+                                            <h3 className="address__item__title">
+                                                {address.name}
+                                            </h3>
+                                            <p className="address__item__link">
+                                                открыть в карте
+                                            </p>
+                                        </Flex>
+
+                                        <ArrowRightOutlined
+                                            style={{ margin: "0 0 0 auto" }}
+                                        />
+                                    </Flex>
+                                </a>
+
+                                <Divider />
+
+                                <Flex gap={5}>
+                                    <EnvironmentOutlined />
+                                    <p className="address__item__text">
+                                        {address.address}
+                                    </p>
+                                </Flex>
+                                <Flex justify="space-between" align="center">
+                                    <Flex align="center" gap={5}>
+                                        <ClockCircleOutlined />
+                                        <p className="address__item__text">
+                                            {address.workTime}
+                                        </p>
+                                    </Flex>
+
+                                    <Flex align="center" gap={5}>
+                                        <PhoneOutlined />
+                                        <p className="address__item__text">
+                                            {address.phoneNumber}
+                                        </p>
+                                    </Flex>
+                                </Flex>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </section>
+    );
 };
 
 export default Address;
--- src/components/news/index.tsx
+++ src/components/news/index.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-const News = () => {
-    return <div>News</div>;
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import img from "../../assets/images/news.jpg";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Button } from "antd";
+
+const News: React.FC = () => {
+    const [news] = useState([
+        { id: 1, text: "На кухонную мебель", title: "50%", image: img },
+        { id: 2, text: "На кухонную мебель", title: "50%", image: img },
+        { id: 3, text: "На кухонную мебель", title: "50%", image: img },
+        { id: 4, text: "На кухонную мебель", title: "50%", image: img },
+    ]);
+
+    return (
+        <section className="news">
+            <div className="container">
+                <h2 className="title">News</h2>
+                <Swiper
+                    modules={[Navigation, Pagination]}
+                    pagination={{ clickable: true, dynamicBullets: true }}
+                    spaceBetween={10}
+                    slidesPerView={3}
+                    navigation
+                    loop
+                >
+                    {news.map((el) => (
+                        <SwiperSlide>
+                            <div className="news__item">
+                                <img
+                                    src={el.image}
+                                    alt=""
+                                    className="news__item__img"
+                                />
+                                <div className="news__item__content">
+                                    <h3 className="news__item__title">
+                                        {el.title}
+                                    </h3>
+                                    <p className="news__item__text">
+                                        {el.text}
+                                    </p>
+                                    <Button type="primary">Подробнее</Button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </section>
+    );
 };
 
 export default News;
